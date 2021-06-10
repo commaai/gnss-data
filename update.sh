d3aa1d98cc2dfd8119a8288429e6d06c6a52f707
Greg Hogan <gregjhogan@gmail.com>
2021-06-09 17:45:14 -0700

    fix ultra-rapid orbit solution name matching

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 cd "$(dirname "$0")"
 
+HH="[0-2][0-9]"
 YY="[0-9][0-9]"
 DOY="[0-9][0-9][0-9]"
 DOW="[0-9]"
@@ -11,7 +12,7 @@ echo "STARTING: /gnss/data/daily/"
 lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/data/daily/202[1-9]/ --directory=/gnss/data/daily/20[3-9][0-9]/ --include-glob=${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].Z --include-glob=${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].gz --target-directory=./gnss/data/daily/;exit" gdc.cddis.eosdis.nasa.gov
 
 echo "STARTING: /gnss/products/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/21[4-9][0-9]/ --directory=/gnss/products/2[2-9][0-9][0-9]/ --directory=/gnss/products/[3-9][0-9][0-9][0-9]/ --include-glob=ig[sru]${GPS_WEEK}${DOW}.sp3.Z --target-directory=./gnss/products/;exit" gdc.cddis.eosdis.nasa.gov
+lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/21[4-9][0-9]/ --directory=/gnss/products/2[2-9][0-9][0-9]/ --directory=/gnss/products/[3-9][0-9][0-9][0-9]/ --include-glob=ig[sr]${GPS_WEEK}${DOW}.sp3.Z --include-glob=igu${GPS_WEEK}${DOW}_${HH}.sp3.Z --target-directory=./gnss/products/;exit" gdc.cddis.eosdis.nasa.gov
 
 echo "STARTING: /gnss/products/ionex/"
 lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/ionex/202[1-9]/ --directory=/gnss/products/ionex/20[3-9][0-9]/ --include-glob=${DOY}/codg${DOY}0.${YY}i.Z -I --include-glob=${DOY}/c[12]pg${DOY}0.${YY}i.Z --target-directory=./gnss/products/ionex/;exit" gdc.cddis.eosdis.nasa.gov
