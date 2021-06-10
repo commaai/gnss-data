b7582ad1035edba7a30547d0f6540147d4e32619
Greg Hogan <gregjhogan@gmail.com>
2021-06-09 18:33:38 -0700

    fix pulling ionosphere 1 and 2 day predicted solutions

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ echo "STARTING: /gnss/products/"
 lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/21[4-9][0-9]/ --directory=/gnss/products/2[2-9][0-9][0-9]/ --directory=/gnss/products/[3-9][0-9][0-9][0-9]/ --include-glob=ig[sr]${GPS_WEEK}${DOW}.sp3.Z --include-glob=igu${GPS_WEEK}${DOW}_${HH}.sp3.Z --target-directory=./gnss/products/;exit" gdc.cddis.eosdis.nasa.gov
 
 echo "STARTING: /gnss/products/ionex/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/ionex/202[1-9]/ --directory=/gnss/products/ionex/20[3-9][0-9]/ --include-glob=${DOY}/codg${DOY}0.${YY}i.Z -I --include-glob=${DOY}/c[12]pg${DOY}0.${YY}i.Z --target-directory=./gnss/products/ionex/;exit" gdc.cddis.eosdis.nasa.gov
+lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/ionex/202[1-9]/ --directory=/gnss/products/ionex/20[3-9][0-9]/ --include-glob=${DOY}/codg${DOY}0.${YY}i.Z --include-glob=${DOY}/c[12]pg${DOY}0.${YY}i.Z --target-directory=./gnss/products/ionex/;exit" gdc.cddis.eosdis.nasa.gov
 
 echo "STARTING: /gnss/products/bias/"
 lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --only-missing --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/bias/202[1-9]/ --directory=/gnss/products/bias/20[3-9][0-9]/ --include-glob=*_DCB.BSX.gz --target-directory=./gnss/products/bias/;exit" gdc.cddis.eosdis.nasa.gov
