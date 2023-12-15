031becc30ecb333b7a08205cf09a5d1553b9af58
Greg Hogan <gregjhogan@gmail.com>
2023-12-15 13:40:35 -0800

    lftp reusable function

--- update.sh
+++ update.sh
@@ -1,6 +1,31 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cd "$(dirname "$0")"
+
+function sync_dir
+{
+  if [ -z $1 ]; then
+    echo "ERROR: target directory value is not set"
+    exit 1
+  fi
+  TARGET_DIR="$1"
+
+  if [ -z $2 ]; then
+    echo "ERROR: source directory array is not set"
+    exit 1
+  fi
+  SOURCE_DIRS=($2) # convert space separated string to array
+
+  if [ -z $3 ]; then
+    echo "ERROR: include glob array is not set"
+    exit 1
+  fi
+  INCLUDE_GLOBS=($3) # convert space separated string to array
+
+  SERVER_HOSTNAME="gdc.cddis.eosdis.nasa.gov"
+  LFTP_MIRROR_OPTS="--no-empty-dirs --no-perms --no-umask --ignore-time --parallel=10"
+  lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror ${LFTP_MIRROR_OPTS} $(echo ${SOURCE_DIRS[@]/#/--directory=}) $(echo ${INCLUDE_GLOBS[@]/#/--include-glob=}) --target-directory=${TARGET_DIR};exit" ${SERVER_HOSTNAME}
+}
 
 HH="[0-2][0-9]"
 YY="[0-9][0-9]"
@@ -9,22 +34,20 @@ DOY="[0-9][0-9][0-9]"
 DOW="[0-9]"
 GPS_WEEK="[0-9][0-9][0-9][0-9]"
 
-CURR_YEAR=$(date -u +%Y)
-PREV_YEAR=$(( ${CURR_YEAR} - 1 ))
-CURR_GPS_WEEK=$(( ( $(date -u +%s) - $(date -u -d'1980-01-06 00:00:00' +%s) ) / 60 / 60 / 24 / 7 ))
-PREV_GPS_WEEK=$(( ${CURR_GPS_WEEK} - 1 ))
+END_YEAR=$(date -u +%Y)
+START_YEAR=$(( ${END_YEAR} - 1 ))
+for CURR_YEAR in $(seq ${START_YEAR} ${END_YEAR}); do
+  echo "STARTING YEAR: ${CURR_YEAR}"
+  #        target dir               source dirs (space separated array)   include globs (space separated array)
+  sync_dir "./gnss/data/daily/"     "/gnss/data/daily/${CURR_YEAR}/"      "${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].Z ${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].gz"
+  sync_dir "./gnss/products/ionex/" "/gnss/products/ionex/${CURR_YEAR}/"  "${DOY}/COD0OPSFIN_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz ${DOY}/COD0OPSRAP_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz"
+  sync_dir "./gnss/products/bias/"  "/gnss/products/bias/${CURR_YEAR}/"   "*_DCB.BSX.gz"
+done
 
-echo "STARTING: /gnss/data/daily/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/data/daily/${CURR_YEAR}/ --directory=/gnss/data/daily/${PREV_YEAR}/ --include-glob=${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].Z --include-glob=${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].gz --target-directory=./gnss/data/daily/;exit" gdc.cddis.eosdis.nasa.gov
-
-echo "STARTING: /gnss/products/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/${CURR_GPS_WEEK}/ --directory=/gnss/products/${PREV_GPS_WEEK}/ --include-glob=ig[sr]${GPS_WEEK}${DOW}.sp3.Z --include-glob=igu${GPS_WEEK}${DOW}_${HH}.sp3.Z --target-directory=./gnss/products/;exit" gdc.cddis.eosdis.nasa.gov
-
-echo "STARTING: /gnss/products/ionex/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/ionex/${CURR_YEAR}/ --directory=/gnss/products/ionex/${PREV_YEAR}/ --include-glob=${DOY}/COD0OPSFIN_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz --include-glob=${DOY}/COD0OPSRAP_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz --target-directory=./gnss/products/ionex/;exit" gdc.cddis.eosdis.nasa.gov
-
-echo "STARTING: /gnss/products/bias/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/gnss/products/bias/${CURR_YEAR}/ --directory=/gnss/products/bias/${PREV_YEAR}/ --include-glob=*_DCB.BSX.gz --target-directory=./gnss/products/bias/;exit" gdc.cddis.eosdis.nasa.gov
-
-echo "STARTING: /glonass/products/"
-lftp -d -u anonymous,admin@comma.ai -e "set ftp:ssl-force true" -e "mirror --no-empty-dirs --no-perms --no-umask --parallel=10 --directory=/glonass/products/${CURR_GPS_WEEK}/ --directory=/glonass/products/${PREV_GPS_WEEK}/ --include-glob=ig[l]${GPS_WEEK}${DOW}.sp3.Z --target-directory=./glonass/products/;exit" gdc.cddis.eosdis.nasa.gov
+END_GPS_WEEK=$(( ( $(date -u +%s) - $(date -u -d'1980-01-06 00:00:00' +%s) ) / 60 / 60 / 24 / 7 ))
+START_GPS_WEEK=$(( ${END_GPS_WEEK} - 52 ))
+for CURR_GPS_WEEK in $(seq ${START_GPS_WEEK} ${END_GPS_WEEK}); do
+  #        target dir             source dirs (space separated array)   include globs (space separated array)
+  sync_dir "./gnss/products/"     "/gnss/products/${CURR_GPS_WEEK}/"    "ig[sr]${GPS_WEEK}${DOW}.sp3.Z igu${GPS_WEEK}${DOW}_${HH}.sp3.Z"
+  sync_dir "./glonass/products/"  "/glonass/products/${CURR_GPS_WEEK}/" "ig[l]${GPS_WEEK}${DOW}.sp3.Z"
+done
