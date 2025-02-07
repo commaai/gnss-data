c21e79cd527c3e2d783130462a3be8cda78a5be5
Bruce Wayne <harald.the.engineer@gmail.com>
2025-02-06 18:02:23 -0800

    Update update

--- update.sh
+++ update.sh
@@ -40,15 +40,14 @@ START_YEAR=$(( ${END_YEAR} - 1 ))
 for CURR_YEAR in $(seq ${START_YEAR} ${END_YEAR}); do
   echo "STARTING YEAR: ${CURR_YEAR}"
   #        target dir               source dirs (space separated array)   include globs (space separated array)
-  sync_dir "./gnss/data/daily/"     "/gnss/data/daily/${CURR_YEAR}/"      "${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].Z ${DOY}/${YY}[ng]/brdc${DOY}0.${YY}[ng].gz"
   sync_dir "./gnss/products/ionex/" "/gnss/products/ionex/${CURR_YEAR}/"  "${DOY}/COD0OPSFIN_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz ${DOY}/COD0OPSRAP_${YYYY}${DOY}0000_01D_01H_GIM.INX.gz ${DOY}/c2pg${DOY}0.${YY}i.Z"
   sync_dir "./gnss/products/bias/"  "/gnss/products/bias/${CURR_YEAR}/"   "*_DCB.BSX.gz *_DCB.BIA.gz"
 done
 
 END_GPS_WEEK=$(( ( $(date -u +%s) - $(date -u -d'1980-01-06 00:00:00' +%s) ) / 60 / 60 / 24 / 7 ))
-START_GPS_WEEK=$(( ${END_GPS_WEEK} - 4 ))
+START_GPS_WEEK=$(( ${END_GPS_WEEK} - 52 ))
 for CURR_GPS_WEEK in $(seq ${START_GPS_WEEK} ${END_GPS_WEEK}); do
   echo "STARTING GPS WEEK: ${CURR_GPS_WEEK}"
   #        target dir             source dirs (space separated array)   include globs (space separated array)
-  sync_dir "./gnss/products/"     "/gnss/products/${CURR_GPS_WEEK}/"    "COD${V}OPSFIN_${YYYY}${DOY}0000_01D_05M_ORB.SP3.gz COD${V}OPSRAP_${YYYY}${DOY}0000_01D_05M_ORB.SP3.gz COD${V}OPSULT_${YYYY}${DOY}${HH}00_02D_05M_ORB.SP3.gz"
+  sync_dir "./gnss/products/"     "/gnss/products/${CURR_GPS_WEEK}/"    "COD${V}MGXFIN_${YYYY}${DOY}0000_01D_05M_ORB.SP3.gz COD${V}OPSFIN_${YYYY}${DOY}0000_01D_05M_ORB.SP3.gz COD${V}OPSRAP_${YYYY}${DOY}0000_01D_05M_ORB.SP3.gz COD${V}OPSULT_${YYYY}${DOY}${HH}00_02D_05M_ORB.SP3.gz"
 done
